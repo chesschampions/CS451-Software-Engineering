90c893ad101783d64e21a37791769b64b87e7791
Chris B <Christian.bakopoulos@gmail.com>
2019-03-11 07:46:19 -0400

    GUI Changes: Tore our all GUI related changes to the datastruture of game.
    Gui only now renders the board structure managed by the backend.
    
    Backend Changes:
    Added move validator replace old algorithm with new one that is a little easier to read.
    Client/Session sharing is working. Were some concurrency issues that needed to be settled.
    
    Final changes:
    Add turn indicator to HTML page.
    Add Comments.
    Add input validation/custom fields.
    Add jumping/taking piece related stuff. (should only take minor bug fixes.)

--- app.js
+++ app.js
@@ -46,24 +46,187 @@ http.listen(3000, function(){
 
 //move a 2d array of xys of the form [[origrow,origcol],[newrow, newcol]]
 //returns the new gameobj
+function equalmoves(x,y) {
+    return x[0] == y[0] && x[1]== y[1];
+}
+
 function makeMove(gameobj,movereq){
     var nextmove = [[movereq[1],movereq[2]], [movereq[3],movereq[4]]];
     var boardchar = gameobj.boardstate[nextmove[0][0]][nextmove[0][1]];
-
     gameobj.boardstate[nextmove[0][0]][nextmove[0][1]] = 0;
     gameobj.boardstate[nextmove[1][0]][nextmove[1][1]] = boardchar;
-    if (gameobj.curPlayer === "X") {gameobj.curPlayer = "O";} else {gameobj.curPlayer = "X";}
+    gameobj.boardstate = checkForKings(gameobj.boardstate);
+    if (gameobj.curPlayer == "X") {gameobj.curPlayer = "O";} else {gameobj.curPlayer = "X";}
     return gameobj;
 }
 
+function convertHashedMove(move){
+    var row = move < 10? 0 : Math.floor(move/ 10);
+    var col = move % 10;
+    return [row,col];
+}
 
-let gameEngine = class gameengine {
-    movevalidator(gameobj, movereq) {
+function isO(piece){ return piece == 1 || piece == 2;}
 
+function checkForMandatoryMoves(player,board){
+    var mandatoryMoves = [];
+
+    for(let piece in getPieces(player,board)){
+        mandatoryMoves.concat(checkForJumps(board,piece[1],piece[2]));
+    }
+    return mandatoryMoves;
+}
+
+function checkForJumps(board, row, col){
+        console.log(board);
+        var Xupmoves = [];
+        var Xdownmoves = [];
+
+        var Oupmoves = [];
+        var Odownmoves = [];
+
+        var maxRow = 6;
+        var minRow = 1;
+        var maxCol = 6;
+        var minCol = 1;
+
+        var downRight = [];
+        var downLeft  = [];
+        var upRight   = [];
+        var upLeft    = [];
+
+        if(row < maxRow && col < maxCol && board[row+2][col+2] != 0){upRight   = [row+2,col+2]; isO(board[row+2][col+2]) ? Oupmoves.push(upRight)  : Xupmoves.push(upRight);}
+        if(row > minRow && col < maxCol && board[row-2][col+2] != 0){downRight = [row-2,col+2]; isO(board[row-2][col+2]) ? Oupmoves.push(downRight): Xupmoves.push(downRight);}
+        if(row < maxRow && col > minCol && board[row+2][col-2] != 0){upLeft    = [row+2,col-2]; isO(board[row+2][col-2]) ? Oupmoves.push(upLeft)   : Xupmoves.push(upLeft);}
+        if(row > minRow && col > minCol && board[row-2][col-2] != 0){downLeft  = [row-2,col-2]; isO(board[row-2][col-2]) ? Oupmoves.push(downLeft) : Xupmoves.push(downLeft);}
+
+        if (board[row][col] == 2){
+            return Oupmoves.concat(Odownmoves);
+        } else if(board[row][col] == 4 ){
+            return Xupmoves.concat(Xdownmoves);
+        } else if(board[row][col]==1){
+            return Xdownmoves;
+        } else if(board[row][col] == 3){
+            return Oupmoves;
+        } else {
+            console.out("Something bad happened with jump moves");
+        }
+}
+function getPieces(player,board){
+    var pieces = [];
+    for(let row in board){
+        for(let col in row){
+            if(col > 0){
+                pieces.push([board[row][col],row,col])
+            }
+        }
+    }
+    if(player == "both"){
+        return pieces;
+    } else if(player == "X"){
+        var opieces = [];
+        for(let piece in pieces){
+            if(piece[0] < 3){
+                opieces.push(piece);
+            }
+        }
+        return opieces;
+
+    } else if(player == "O"){
+        var xpieces = [];
+        for(let piece in pieces){
+            if(piece[0] >= 3){
+                xpieces.push(piece);
+            }
+        }
+        return xpieces;
+
+    } else {
+        console.log("bad getPieces req");
+    }
+}
+function checkForMoves(board, row, col){
+    console.log("Piece were looking at ", board[row][col]);
+    var upmoves =[];
+    var downmoves = [];
+    var maxRow = 7;
+    var minRow = 0;
+    var maxCol = 7;
+    var minCol = 0;
+
+    var downRight = [];
+    var downLeft  = [];
+    var upRight   = [];
+    var upLeft    = [];
+
+    if(row < maxRow && col < maxCol && board[row+1][col+1] == 0){upRight   = [row+1,col+1]; upmoves.push(upRight);}
+    if(row > minRow && col < maxCol && board[row-1][col+1] == 0){downRight = [row-1,col+1]; downmoves.push(downRight);}
+    if(row < maxRow && col > minCol && board[row+1][col-1] == 0){upLeft    = [row+1,col-1]; upmoves.push(upLeft);}
+    if(row > minRow && col > minCol && board[row-1][col-1] == 0){downLeft  = [row-1,col-1]; downmoves.push(downLeft);}
+
+    console.log("MOVES: " + upmoves.concat(downmoves)[0]);
+    if (board[row][col] == 2 || board[row][col] == 4 ){
+        return upmoves.concat(downmoves);
+    } else if(board[row][col]==1){
+        return upmoves;
+    } else if(board[row][col] == 3){
+        return downmoves;
+    } else {
+        console.log("Something bad happened with moves");
+        return [];
+    }
+}
+function checkForKings(board) {
+    for (let piece in board[7]) {
+        if (piece == 1) {
+            piece = 2;
+        }
     }
 
+    for (let piece in board[0]) {
+        if (piece == 3) {
+            piece = 4;
+        }
+    }
+    return board;
+}
 
-    mandatoryJump(player){
+//TODO: Move validation
+function movevalidator(gameobj, movereq) {
+    console.log(movereq[0]);
+    console.log(gameobj.curPlayer);
+    if(gameobj.curPlayer != movereq[0]){return false;}
+
+    console.log("Passed Player Check");
+
+    var origpos = [movereq[1],movereq[2]];
+    var newpos  = [movereq[3],movereq[4]];
+
+    var board = gameobj.boardstate;
+    var player = movereq[0];
+
+    var mandatoryMoves =checkForMandatoryMoves(player,gameobj.boardstate);
+    console.log("Done with mandatory moves");
+    var otherMoves = checkForMoves(gameobj.boardstate, origpos[0], origpos[1]);
+
+    if(mandatoryMoves.length > 0){
+        for(var i = 0; i < mandatoryMoves.length; i++) {
+            if (equalmoves(mandatoryMoves[i],newpos) == true) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        for(var i = 0; i < otherMoves.length; i++) {
+            if (equalmoves(otherMoves[i],newpos) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function mandatoryJump(player){
         var allPieces = document.getElementsByClassName("black");
         var myPieces = [];
 
@@ -93,15 +256,13 @@ let gameEngine = class gameengine {
         }
         return false;
     }
-}
 
 //TODO: Add try catch blocks to open and save sessions.
-function openSession(gamefilename,roomid){
+function openSession(roomid){
     console.log("opening session " + roomid);
-    fs.readFile(__dirname + "/Sessions/" + roomid, function(err,gameObjBuff){
-        console.log("Loaded!");
-        return JSON.parse(gameObjBuff.toString());
-    });
+    var gameobj = fs.readFileSync(__dirname + "/Sessions/" + roomid, function(err,gameObjBuff){});
+    console.log("Loaded!");
+    return JSON.parse(gameobj.toString());
 }
 function saveSession(gameObj, roomid){
     console.log("saving session " + roomid);
@@ -116,7 +277,6 @@ io.on('connection', function(socket){
     console.log("user connected ");
     //Fresh board is generated.
     var isgame = false;
-
     var game = {
         curPlayer : "O",
         boardstate : [
@@ -136,11 +296,11 @@ io.on('connection', function(socket){
         if(msg === -1){
             console.log("Got a new game request");
             roomid=sessionCount;
-            socket.join(roomid);
+            socket.join(roomid.toString());
             console.log("Added session and made a new io room");
             // emits the game id back to the client
             socket.emit("gid", roomid);
-            socket.emit("playerName","X");
+            socket.emit("playerName","O");
             console.log("emitted playername and gid");
             saveSession(game,roomid);
             sessionCount++;
@@ -158,10 +318,14 @@ io.on('connection', function(socket){
                         console.log(parseInt(session));
                         roomid = parseInt(session);
 
-                        socket.emit("playerName","O");
+                        socket.emit("playerName","X");
                         socket.emit("gid", roomid);
-                        socket.join(roomid);
-                        openSession(game,roomid);
+                        socket.join(roomid.toString());
+
+                        console.log(openSession(roomid));
+                        game = openSession(roomid);
+
+                        io.in(roomid.toString()).emit("updateBoard",game.boardstate);
                         isgame = true;
                         console.log("setup session");
                     }
@@ -178,20 +342,21 @@ io.on('connection', function(socket){
 
     //Moves done through callback functions and promises.
     socket.on("moveReq", function(msg,fn){
+        if(isgame){
         //temp value for addedmove
          //var validmove = gameengine.movevalidator(msg);
         console.log("Recived Move Request");
-        if(true){
+        game = openSession(roomid.toString());
+        if(movevalidator(game,msg)){
             game = makeMove(game,msg);
             console.log("sending move res");
-            socket.to(roomid).emit("updateBoard", [game.curPlayer, game.boardstate]);
-            fn(true);
-            //socket.emit("moveRes",true);
+            io.in(roomid.toString()).emit("updateBoard", game.boardstate);
+            saveSession(game,roomid);
+            //fn(true);
         } else {
-            fn(false);
-            //socket.emit("moveRes",false);
+            //fn(false);
             socket.emit("clientError","bad move");
-        }
+        }}
     });
 
     //Disconnection Handler
@@ -203,7 +368,7 @@ io.on('connection', function(socket){
         console.log("user disconnected, saving game");
         saveSession(game,roomid);
         console.log("sending error to other player.");
-        socket.to(roomid).emit("oppoentDC","The opponent left")
+        io.in(roomid.toString()).emit("oppoentDC","The opponent left")
         }
 
     });
