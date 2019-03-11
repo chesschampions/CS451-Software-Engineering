262b57f41134c7675f397a74a99523275995524a
Chris B <Christian.bakopoulos@gmail.com>
2019-03-11 02:50:01 -0400

    Fixed serialization, user sessions, and movement, made code compatible on index.html with prev GUI code.

--- gameengine.js
+++ gameengine.js
@@ -1,13 +1,24 @@
 class gameengine {
     movevalidator(gameobj, movereq) {
-        if gameobj.curPlayer = move[0];
 
     }
-    //Move is a three element list of the form [[curx,cury],[nextx,nexty],player] where player can be 1/2 or -1/-2. (depending on if it's a king piece of not). board
+
+    //move a 2d array of xys of the form [[origrow,origcol],[newrow, newcol]]
+    //returns the new gameobj
+    makeMove(gameobj,movereq){
+        var nextmove = [[movereq[1],movereq[2]], [movereq[3],movereq[4]]];
+        var boardchar = gameobj.boardstate[nextmove[0][0]][nextmove[0][1]];
+
+        gameobj.boardstate[nextmove[0][0]][nextmove[0][1]] = 0;
+        gameobj.boardstate[nextmove[1][0]][nextmove[1][1]] = boardchar;
+        if (gameobj.curPlayer === "X") {gameobj.curPlayer = "O";} else {gameobj.curPlayer = "X";}
+        return gameobj;
+    }
 
     mandatoryJump(player){
             var allPieces = document.getElementsByClassName("black");
             var myPieces = [];
+
             if(player === "O"){
                 for(i = 0; i<allPieces.length; i++){
                     if(allPieces[i].textContent === "O")
@@ -35,4 +46,3 @@ class gameengine {
             return false;
         }
     }
-}
